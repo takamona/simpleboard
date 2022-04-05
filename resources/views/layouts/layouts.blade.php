152005fb358cf24ff39fb1666d37e467d3628555
TakahisaKobayashi <takavyd2720ray@docomo.ne.jp>
2022-04-05 17:50:26 +0900

    deploy to heroku

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -3,7 +3,7 @@
         <title>@yield('title')</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         @component('components.header')
